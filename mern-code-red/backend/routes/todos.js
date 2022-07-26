8328433d9e2010357d448fc7fc94269812312a8b
George Dainton <gcdainton94@gmail.com>
2022-07-26 23:57:49 +0100

    To Do CRUD finished

--- mern-code-red/backend/routes/todos.js
+++ mern-code-red/backend/routes/todos.js
@@ -3,8 +3,8 @@ let Todo = require('../models/todo.model')
 
 router.route('/').get((req, res) => {
   Todo.find() // built in mongoose method
-    .then(potatoes => res.json(potatoes)) //if successful, take the res(ult) whcih can be called anything, and send it as json
-    .catch(err => res.status(400).json('Error:' + err)); //in error, send as json
+    .then(todo => res.json(todo)) //if successful, take the res(ult) whcih can be called anything, and send it as json
+    .catch(err => res.status(400).json('Error: ' + err)); //in error, send as json
 })
 
 router.route('/add').post((req, res) => {
@@ -26,9 +26,36 @@ router.route('/add').post((req, res) => {
 
   newTodo.save()
     .then(() => res.json('To Do added'))
-    .catch(err => res.status(400).json('Error:' + err));
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  Todo.findById(req.params.id) //not req.body as this is input by the user, params are from the URL, using to find by ID
+    .then(todo => res.json(todo))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Todo.findByIdAndDelete(req.params.id)
+    .then(() => res.json('To Do deleted'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Todo.findById(req.params.id)
+    .then(todo => {
+      todo.username = req.body.username;
+      todo.description = req.body.description;
+      todo.catagory = req.body.catagory;
+      todo.priority = Number(req.body.priority);
+      todo.date = Date.parse(req.body.date);
+    
+      todo.save()
+        .then(() => res.json('To Do updated'))
+        .catch(err => res.status(400).json('Error ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
 
 module.exports = router;
--- mern-code-red/backend/routes/users.js
+++ mern-code-red/backend/routes/users.js
@@ -4,8 +4,8 @@ let User = require('../models/user.model')
 
 router.route('/').get((req, res) => {
   User.find() // built in mongoose method
-    .then(bananas => res.json(bananas)) //if successful, take the res(ult) whcih can be called anything, and send it as json
-    .catch(err => res.status(400).json('Error:' + err)); //in error, send as json
+    .then(todo => res.json(todo)) //if successful, take the res(ult) whcih can be called anything, and send it as json
+    .catch(err => res.status(400).json('Error: ' + err)); //in error, send as json
 })
 
 router.route('/add').post((req, res) => {
@@ -15,7 +15,7 @@ router.route('/add').post((req, res) => {
 
   newUser.save()
     .then(() => res.json('User added'))
-    .catch(err => res.status(400).json('Error:' + err));
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
